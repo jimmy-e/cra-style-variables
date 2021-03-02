1fb68c4ada6b9280d88dd92761ca886f21539eba
Jimmy Emery <jimmy.e@outlook.com>
2021-03-02 15:06:09 -0500

    updating

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SampleComponent from './SampleComponent/SampleComponent';
-import './styles/sizes.scss';
 
 ReactDOM.render(
   <React.StrictMode>
