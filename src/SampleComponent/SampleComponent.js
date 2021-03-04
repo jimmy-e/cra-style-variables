72efc2d21d64b87b8e76d1d47c9ec2d28c4103b3
Jimmy Emery <jimmy.e@outlook.com>
2021-03-04 18:27:46 -0500

    removing

--- src/SampleComponent/SampleComponent.js
+++
@@ -1,9 +0,0 @@
-import * as styles from './sampleComponent.scss';
-
-const SampleComponent = () => (
-  <div className={styles.div}>
-    <h3>hello world</h3>
-  </div>
-);
-
-export default SampleComponent;
